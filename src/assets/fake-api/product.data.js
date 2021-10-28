b78401ce6062467a5190c1934dac24b561895dd0
minhlezz <kuco007@gmail.com>
2021-10-28 09:09:01 +0700

    commit product data

---
+++ src/assets/fake-api/product.data.js
@@ -0,0 +1,123 @@
+const product_01_img_01 = "https://camposcoffee.com/wp-content/uploads/2018/11/500G_Front_SUPERIOR-300x300.png";
+const product_02_img_01 = "https://camposcoffee.com/wp-content/uploads/2016/09/500G_Front_DARK_CITY-300x300.png";
+const product_03_img_01 = "https://camposcoffee.com/wp-content/uploads/2016/09/500G_Front_ROMA-300x300.png";
+const product_04_img_01 = "https://camposcoffee.com/wp-content/uploads/2016/09/500G_Front_DECAF-300x300.png";
+const product_05_img_01 = "https://camposcoffee.com/wp-content/uploads/2016/09/500G_Front_ORGANIC-300x300.png";
+const product_06_img_01 = "https://camposcoffee.com/wp-content/uploads/2016/09/500G_Front_Blade-Runner-1-300x300.png";
+const product_07_img_01 = "https://camposcoffee.com/wp-content/uploads/2021/03/Subscription-300x300.png";
+const product_08_img_01 = "https://camposcoffee.com/wp-content/uploads/2021/07/KENYA_Thirikwa_Gakuyuini-300x300.png";
+const product_09_img_01 = "https://camposcoffee.com/wp-content/uploads/2021/08/colombia-300x300.png";
+const product_10_img_01 = "https://camposcoffee.com/wp-content/uploads/2021/09/Giesha-Heirloom-300x300.png";
+const product_11_img_01 = "https://camposcoffee.com/wp-content/uploads/2021/10/Colombia_Linarco-Rodriguez-300x300.png";
+const product_12_img_01 = "https://www.laviet.coffee/wp-content/uploads/2018/10/Balance.png";
+
+const products = [
+    {
+        title: "CAMPOS SUPERIOR BLEND",
+        price: 15.50,
+        image01: product_01_img_01,
+        categorySlug: "coffee",
+        slug: "campos-superior-blend",
+        size: ["s", "m", "l"],
+        description: "Caramelly & Rich Blend. Sweet like Butterscotch. Flavours like Red Fruit & Apricot."
+    },
+    {
+        title: "DARK CITY",
+        price: 15.50,
+        image01: product_02_img_01,
+        categorySlug: "coffee",
+        slug: "dark-city-blend",
+        size: ["s", "m", "l"],
+        description: "Balanced & Hearty. Sweet like Belgian Chocolate. Flavours like Forest Fruits."
+    },
+    {
+        title: "Roma Espresso Blend",
+        price: 15.50,
+        image01: product_03_img_01,
+        categorySlug: "coffee",
+        slug: "roma-espresso-blend",
+        size: ["s", "m", "l"],
+        description: "Strong & Bold. Sweet like Molasses. Flavours like Chocolate Milk."
+    },
+    {
+        title: "Swiss Water Process® Decaffeinated Espresso",
+        price: 16.50,
+        image01: product_04_img_01,
+        categorySlug: "coffee",
+        slug: "swiss-water-process-decaffeinated-espresso-blend",
+        size: ["s", "m", "l"],
+        description: "Syrupy & Bold. Sweet like Malt Chocolate. Flavours like Real Coffee."
+    },
+    {
+        title: "Organic Espresso Blend",
+        price: 16.50,
+        image01: product_05_img_01,
+        categorySlug: "coffee",
+        slug: "organic-espresso",
+        size: ["s", "m", "l"],
+        description: "Creamy & Balanced. Sweet like Honey. Flavours like Chocolate Orange."
+    },
+    {
+        title: "Blade Runner Blend",
+        price: 16.00,
+        image01: product_06_img_01,
+        categorySlug: "coffee",
+        slug: "blade-runner-blend",
+        size: ["s", "m", "l"],
+        description: "Juicy & Vibrant. Sweet like Toffee. Flavours like Tropical Fruit."
+    },
+    {
+        title: "Campos Subscription",
+        price: 15.00,
+        image01: product_07_img_01,
+        categorySlug: "coffee",
+        slug: "campos-subscription",
+        size: ["s", "m", "l"],
+        description: "Our freshly roasted coffee delivered right to your door"
+    },
+    {
+        title: "Kenya Thirikwa Gakuyuini",
+        price: 20.05,
+        image01: product_08_img_01,
+        categorySlug: "coffee",
+        slug: "kenya-thirikwa-gakuyuini-coffee",
+        size: ["s", "m", "l"],
+        description: "Sweet like Maple Syrup. Flavours like Black Currant and Lemon"
+    },
+    {
+        title: "Colombia El Quebradon",
+        price: 16.00,
+        image01: product_09_img_01,
+        categorySlug: "coffee",
+        slug: "colombia-el-quebradon-2",
+        size: ["s", "m", "l"],
+        description: "Tastes like raisin and almond. Sweet like chocolate fudge"
+    },
+    {
+        title: "Geisha Heirloom",
+        price: 21.00,
+        image01: product_10_img_01,
+        categorySlug: "coffee",
+        slug: "geisha-heirloom-october",
+        size: ["s", "m", "l"],
+        description: "Tastes like Hibiscus & Lychee, sweet like Panela"
+    },
+    {
+        title: "Colombia Linarco Rodriguez Ospina",
+        price: 24.00,
+        image01: product_11_img_01,
+        categorySlug: "coffee",
+        slug: "colombia-linarco-rodriguez-ospina",
+        size: ["s", "m", "l"],
+        description: "Tastes like Peach and Pineapple, sweet like Honey"
+    },
+    {
+        title: "Vietnamese Balanced",
+        price: 9.99,
+        image01: product_12_img_01,
+        categorySlug: "coffee",
+        slug: "vietnamese-balanced",
+        size: ["s", "m", "l"],
+        description: "Flavors of raw honey, sage and barrel oak with a milky, bourbon-like texture."
+    }
+]
---
+++ src/hooks/useFetchHook.js
@@ -0,0 +1,12 @@
+import { useState, useCallback, useEffect } from "react";
+
+const useFetch = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [list, setList] = useState([]);
+
+    
+}
+
+
+export default useFetch
